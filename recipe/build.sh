f19b62638fe6cba573b009721f5f18012d3cca82
milesgranger <miles59923@gmail.com>
2021-02-19 15:32:31 +0100

    parameterize with jinja using pypi.debian

--- recipe/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-echo "Installing cramjam==$PKG_VERSION"
-pip install --index-url https://pypi.org/simple cramjam==$PKG_VERSION
-#pip install https://files.pythonhosted.org/packages/87/a1/63d406857da2f944768f086dcbe22e712901187ddf3f511182455337b03a/cramjam-2.0.0-cp37-cp37m-manylinux2010_x86_64.whl
