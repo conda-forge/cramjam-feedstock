a9c6db9b3af379ba67275a22319d16b2b63b2236
milesgranger <miles59923@gmail.com>
2021-02-19 14:24:35 +0100

    TEST: linux py37 - install directly from wheels url

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,4 @@
 set -xe
 
 echo "Installing cramjam==$PKG_VERSION"
-python -m pip install --no-deps cramjam==$PKG_VERSION
+pip install https://files.pythonhosted.org/packages/87/a1/63d406857da2f944768f086dcbe22e712901187ddf3f511182455337b03a/cramjam-2.0.0-cp37-cp37m-manylinux2010_x86_64.whl
